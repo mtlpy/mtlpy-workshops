41691f80c507d838a6cdb562e077b8a216aad4a8
Jordi Gutiérrez Hermoso <jordigh@octave.org>
2013-10-16 21:11:30 -0400

    Add English version of script.py

---
+++ python-basic/sup.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+def sup(name):
+    return u"Sup %s!" % (nom)
+
+if __name__ == '__main__':
+    print u"--------------------------------------------------"
+    print u"START the script"
+    print u"--------------------------------------------------"
+    name = raw_input("What is your name? ")
+    print sup(name)
+    print u"-----------------------------------------------"
+    print u"END the script"
+    print u"-------------------------------------------------"
